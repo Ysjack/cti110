3f24e742155cbd7e596cee3d6f876c14be1eb5dd
Ysjack <103299579+Ysjack@users.noreply.github.com>
2022-04-12 18:07:53 -0400

    Add files via upload

--- P1HW2_McdougalYashua.py
+++ P1HW2_McdougalYashua.py
@@ -1,18 +1,17 @@
-Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
 #I am using  IDLE to create and test code involving calculating the number of pizzas you need to order for a group of students.
 #4/9/2022
 #CTI-110 P1HW2 - Pizza Order
 #Yashua Mcdougal
 #
->>>students = int(input(20))
->>>pizzas slices = int(input(60))
->>>pizzas = int(input(10))
+students = 20
+pslices = 60
+pizzas = 10
 
-print('How many students do you want to order pizza for?', students)
-print()
+
+print('How many students do you want to order pizza for?,', students)
+
 print('----Pizza Order--------')
 print('Number of Students :', students)
-print('Pizza Slices Needed:', pizzas slices)
+print('Pizza Slices Needed:', pslices)
 print('Pizzas Needed :', pizzas)
-print("-------------------------")
+print('-------------------------')
